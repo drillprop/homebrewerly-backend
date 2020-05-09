62df129964aa982bf40fcb5e3e56f620a611ee44
drillprops <drylbartosz@gmail.com>
2020-05-09 23:08:43 +0200

    create app class and controller interface

---
+++ src/app.ts
@@ -0,0 +1,32 @@
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import Controller from 'interfaces/controller.interface';
+
+export default class App {
+  public app: express.Application;
+  public port: number;
+
+  constructor(controllers: Controller[], port: number) {
+    this.app = express();
+    this.port = port;
+
+    this.initializeMiddlewares();
+    this.initializeControllers(controllers);
+  }
+
+  private initializeMiddlewares() {
+    this.app.use(bodyParser.json());
+  }
+
+  private initializeControllers(controllers: Controller[]) {
+    controllers.forEach((controller) => {
+      this.app.use('/', controller.router);
+    });
+  }
+
+  public listen() {
+    this.app.listen(this.port, () => {
+      console.log(`server started at port ${this.port}`);
+    });
+  }
+}
