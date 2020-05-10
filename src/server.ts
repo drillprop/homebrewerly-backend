3641c2471a5070111d0e4967f3c0d9929463f60f
drillprops <drylbartosz@gmail.com>
2020-05-10 11:47:14 +0200

    add validateEnv and import dotenv in server

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
+import 'dotenv/config';
 import App from './app';
 import RecipesController from './recipes/recipe.controller';
+import validateEnv from './utils/validateEnv';
+
+validateEnv();
 
 const app = new App([new RecipesController()], 5000);
 
