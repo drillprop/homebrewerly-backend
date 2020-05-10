6e2c65284b875b53270befac1c28ce9e009a0db6
drillprops <drylbartosz@gmail.com>
2020-05-10 22:15:40 +0200

    add AuthController to App's argument array

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import 'dotenv/config';
 import App from './app';
+import AuthController from './auth/auth.controller';
 import RecipesController from './recipes/recipe.controller';
 import validateEnv from './utils/validateEnv';
 
 validateEnv();
 
-const app = new App([new RecipesController()]);
+const app = new App([new AuthController(), new RecipesController()]);
 
 app.listen();
